052ea5c46d93a12f7df61f05d9e2df39522156c3
Brian Estrada <bestr2015@gmail.com>
2023-12-21 15:42:52 -0800

    Add authentication logic to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store.js";
+import supabase from "../supaBase.js";
 import "./App.css";
 import Login from "./Login.jsx";
 import Navbar from "./Navbar.jsx";
@@ -11,26 +13,44 @@ import PreviousWorkouts from "./PreviousWorkouts.jsx";
 import Progresssion from "./Progression.jsx";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const session = supabase.auth.getSession();
+
+    setIsAuthenticated(!!session);
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setIsAuthenticated(!!session);
+    });
+  }, []);
+
   return (
     <>
       <Provider store={store}>
         <Router>
           <Navbar></Navbar>
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/workout" element={<Workout />}></Route>
-            <Route path="/createWorkouts" element={<CreateWorkouts />}></Route>
-            <Route
-              path="/previousWorkouts"
-              element={<PreviousWorkouts />}
-            ></Route>
-            <Route path="/progression" element={<Progresssion />}></Route>
+            {isAuthenticated ? (
+              <>
+                <Route path="/home" element={<Home />} />
+                <Route path="/workout" element={<Workout />}></Route>
+                <Route
+                  path="/createWorkouts"
+                  element={<CreateWorkouts />}
+                ></Route>
+                <Route
+                  path="/previousWorkouts"
+                  element={<PreviousWorkouts />}
+                ></Route>
+                <Route path="/progression" element={<Progresssion />}></Route>
+              </>
+            ) : (
+              <Route path="/" element={<Login />} />
+            )}
           </Routes>
         </Router>
       </Provider>
     </>
   );
 }
-
 export default App;
