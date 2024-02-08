5441b8ef6d88be664d0f54a6c6ed707b15fa6158
Brian Estrada <bestr2015@gmail.com>
2024-02-08 14:15:07 -0800

    Add chart.js library and implement initial fetch for sessions and exercises for stats

--- src/Progression.jsx
+++ src/Progression.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchSessions } from "./helpers/previousWorkouts";
+import SessionStats from "./SessionStats";
+
 function Progression() {
+  const [sessions, setSessions] = useState([]);
+  useEffect(() => {
+    //Here is what I need to do!
+    // I want to make these fetches in a helper file to keep this compact
+    // I need to fetch sessions and exercises from the database
+    // I need to default the sessions retrieved to 1 month
+    //Sessions will be easy not a complex call exercises ill need to join exercise_sets and exercises
+    // As an input these functions will take in a date range parameter but it will default to the last month
+    // if this parameter is updated (useEffect Dependency) I will need to make another call to the database
+    // Once I have thee data in objects I can pass them to the SessionStats component and ExerciesStats components
+    //Where I can implement more helper functions to calculate the data I need to aggergate and display
+    // Also formate data such that it can be used in the chart.js library
+  }, []);
   return (
     <div>
       <h1>Progression</h1>
       <Link to="/">Back</Link>
+      <SessionStats />
     </div>
   );
 }
---
+++ src/SessionStats.jsx
@@ -0,0 +1,64 @@
+import { useRef, useEffect, useState } from "react";
+import { Chart } from "chart.js/auto";
+import BarController from "chart.js/controllers/barController";
+import LinearScale from "chart.js/scales/linearScale";
+import CategoryScale from "chart.js/scales/categoryScale";
+import BarElement from "chart.js/elements/barElement";
+
+Chart.register(BarController, LinearScale, CategoryScale, BarElement);
+
+function SessionStats() {
+  const canvasRef = useRef(null);
+  // duration will be data and is an array where each entry is an object with the duration
+  // in minutes and the date of the session
+  const [duration, setDuration] = useState([]);
+  const [totalDuration, setTotalDuration] = useState(0);
+  const [weightMoved, setWeightMoved] = useState([]);
+  const [totalWeightMoved, setTotalWeightMoved] = useState(0);
+  const [daysWorkedOut, setDaysWorkedOut] = useState([]);
+  const [totalDaysWorkedOut, setTotalDaysWorkedOut] = useState(0);
+  const [stat, setStat] = useState(totalWeightMoved);
+
+  useEffect(() => {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    new Chart(canvasRef.current, {
+      type: "bar",
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: "Acquisitions by year",
+            data: data.map((row) => row.count),
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            type: "linear",
+            beginAtZero: true,
+          },
+          x: {
+            type: "category",
+          },
+        },
+      },
+    });
+  }, [stat]);
+
+  return <canvas ref={canvasRef} />;
+}
+
+export default SessionStats;
